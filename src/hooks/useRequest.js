4df6ab15a576c77d8fb9274f202b0bd74f32af3b
ZGH <zgh0711@163.com>
2024-07-02 09:46:22 +0800

    update

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,28 +1,32 @@
-import { ref } from 'vue'
 import http from '@/utils/http.js'
 
 export function useRequest() {
   const loading = ref(false)
 
-  function get(url, successCallback, params = {}) {
-    request('get', url, successCallback, params)
+  function get(url, successCallback = () => {}, params = {}) {
+    request('get', url, params, successCallback)
   }
 
-  function post(url, successCallback, params = {}) {
-    request('post', url, successCallback, params)
+  function post(url, params = {}, successCallback = () => {}, failCallback = () => {}) {
+    request('post', url, params, successCallback, failCallback)
   }
 
-  function request(method, url, successCallback, params = {}) {
+  function request(method, url, params, successCallback, failCallback = () => {}) {
     loading.value = true
     http
       .request(method, url, params)
       .then(res => {
         if (res.code === 0) {
           successCallback(res.data || null)
+        }else {
+          failCallback(res)
         }
         loading.value = false
       })
       .catch(err => {
+        console.error(`请求失败，URL: ${url}`)
+        console.error(err)
+        failCallback(err)
         loading.value = false
       })
   }
